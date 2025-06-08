f3898256e8ec097c7eefce53d5dbe9ef460b38dd
GokuOp7 <153523489+GokuOp7@users.noreply.github.com>
2025-06-08 09:11:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -23,7 +23,7 @@ try:
     # APP_ID = ""
     API_HASH = config("API_HASH", "622bba3cf046315531f71f9d97fa6c2a")
     # API_HASH = ""
-    BOT_TOKEN = config("BOT_TOKEN", "6200331513:AAEGrlqEg9jiZDkoetp9FnBj_Q61tVSAaG8")
+    BOT_TOKEN = config("BOT_TOKEN", "5410007314:AAGeIGKprHB9EXvSIG68GC2mgr87JP4MG5Q")
     # BOT_TOKEN = ""
     DEV = 5385471287
     OWNER = config("OWNER", "5385471287")
